8727a37282176eb6478ec588e8952b911b78298c
sanghak <sanghk32@naver.com>
2026-04-08 19:11:42 +0900

    construction: 대시보드 최근거래 요약  기능 구현 중

---
+++ src/components/RecentTransactions.vue
@@ -0,0 +1,35 @@
+<template>
+  <div style="height: 91vh; width: 99vw">
+    <div class="d-flex justify-content-center gap-5">
+      <div
+        class="card"
+        style="width: 18rem; height: 10rem; border-radius: 20px"
+      >
+        <div class="card-body text-center">
+          <h5 class="card-title">이번달 수입</h5>
+          <h3 class="card-subtitle text-primary mt-3">5,000,000원</h3>
+        </div>
+      </div>
+      <div class="card" style="width: 18rem; border-radius: 20px">
+        <div class="card-body text-center">
+          <h5 class="card-title">이번달 지출</h5>
+          <h3 class="card-subtitle mt-3 text-danger">5,000,000원</h3>
+        </div>
+      </div>
+      <div class="card" style="width: 18rem; border-radius: 20px">
+        <div class="card-body text-center">
+          <h5 class="card-title">이번달 수입</h5>
+          <h3 class="card-subtitle mt-3 text-body-secondary">5,000,000원</h3>
+        </div>
+      </div>
+      <div class="card" style="width: 18rem; border-radius: 20px">
+        <div class="card-body text-center">
+          <h5 class="card-title">금일 지출</h5>
+          <h3 class="card-subtitle mt-3 text-body-secondary">30,000원</h3>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup></script>
+<style></style>
