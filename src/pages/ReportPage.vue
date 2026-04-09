8390409e54e8085db5253a41a327eb1b32ed4921
JNMYNG <jmj00327@gmail.com>
2026-04-09 09:48:47 +0900

    feat: transactions 데이터 조회 연동

--- src/pages/ReportPage.vue
+++ src/pages/ReportPage.vue
@@ -1,7 +1,37 @@
 <template>
   <div>
     <h1>리포트 페이지</h1>
+
+    <p>거래 개수: {{ transactions.length }}</p>
+
+    <ul>
+      <li v-for="item in transactions" :key="item.id">
+        {{ item.date }} / {{ item.type }} / {{ item.category }} /
+        {{ item.amount }}원
+      </li>
+    </ul>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+
+const transactions = ref([]);
+
+const fetchTransactions = async () => {
+  try {
+    const response = await axios.get(
+      'http://localhost:3000/transactions?userId=1',
+    );
+    transactions.value = response.data;
+    console.log('거래 데이터:', response.data);
+  } catch (error) {
+    console.error('거래 데이터 불러오기 실패:', error);
+  }
+};
+
+onMounted(() => {
+  fetchTransactions();
+});
+</script>
