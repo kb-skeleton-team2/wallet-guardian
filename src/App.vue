27cdd08cea85588586dad6edbf168f5db2a5b247
catlover <catlover@gmail.com>
2026-04-09 10:28:02 +0900

    feat: 공통 헤더 컴포넌트 구현

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,11 @@
 <template>
-  <div>
-    <nav
-      style="
-        display: flex;
-        gap: 20px;
-        padding: 20px;
-        border-bottom: 1px solid #ccc;
-      "
-    >
-      <router-link to="/dashboard">대시보드</router-link>
-      <router-link to="/transactions">거래</router-link>
-      <router-link to="/report">리포트</router-link>
-      <router-link to="/report">마이페이지</router-link>
-    </nav>
+  <Header />
 
+  <div>
     <router-view />
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import Header from '@/components/common/Header.vue';
+</script>
